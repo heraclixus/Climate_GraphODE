afaf1871d368fdd4f87d6ebc3be43c0e55bb1e9d
heraclixus <xuzeyuanfred@ucla.edu>
2024-05-09 22:01:55 -0700

    fix spectrum energy plot

--- src/scripts/visualize_fourier_modes.py
+++ src/scripts/visualize_fourier_modes.py
@@ -14,66 +14,84 @@ from torch_harmonics import RealSHT
 Helper functions 
 """
 
-def obtain_freq_vars(x, x_pred, idx, type="fft"):
-    if type == "fft":
-        x_hat = torch.fft.fftshift(x, dim=(-2,-1))[:, idx, :, :]
-        x_hat_pred = torch.fft.fftshift(x_pred, dim=(-2,-1))[:, idx, :, :]
-    else: # sht
-        x_hat = x[:, idx, :, :]
-        x_hat_pred = x_pred[:, idx, :, :]
-    spectrum = torch.abs(x_hat)
-    spectrum_pred = torch.abs(x_hat_pred)
-    amplitude_mean = spectrum[:, -1, :].mean(dim=0).cpu().numpy()
-    amplitude_std = spectrum[:, -1, :].std(dim=0).cpu().numpy()
-    amplitude_mean_pred = spectrum_pred[:, -1, :].mean(dim=0).cpu().numpy()
-    amplitude_std_pred = spectrum_pred[:, -1, :].std(dim=0).cpu().numpy()
-    N = spectrum.shape[-1]
-    
-    if type == "fft":
-        wavenumbers = torch.fft.fftshift(torch.fft.fftfreq(N)).numpy()
-        wavenumbers *= N
+"""
+update: use mean over minibatch, and provide the power spectrum (energy) plot
+see: https://heraclixus.github.io/mode_vis/ 
+"""
+def obtain_spectrum_fft(x, x_pred, idx, n_bins=32):
+    x_hat = torch.fft.fftn(x, dim=(-2,-1))[idx, :, :]
+    x_hat_pred = torch.fft.fftn(x_pred, dim=(-2,-1))[idx, :, :]
+    x_hat_centered = torch.fft.fftshift(x_hat, dim=(-2,-1))
+    x_hat_centered_pred = torch.fft.fftshift(x_hat_pred, dim=(-2,-1))
+    wavenumbers = torch.abs(x_hat-x_hat_centered).flatten()
+    wavenumbers_pred = torch.abs(x_hat_pred - x_hat_centered_pred).flatten()
+    amptliude = torch.abs(x_hat)
+    amplitude_pred = torch.abs(x_hat_pred)
+    energy, energy_pred = amptliude ** 2, amplitude_pred ** 2 
+    energy, energy_pred = energy.flatten(), energy_pred.flatten()
+    max_wavenumber = max([torch.max(wavenumbers), torch.max(wavenumbers_pred)])
+    bins = torch.arange(0, max_wavenumber, torch.div(max_wavenumber, n_bins, rounding_mode="floor"))
+    if torch.max(wavenumbers) > torch.max(wavenumbers_pred):
+        indices = torch.bucketize(wavenumbers, bins)
     else:
-        wavenumbers = [i+1 for i in range(N)]
-    
-    wavenumbers = np.concatenate([wavenumbers, wavenumbers])
-    preds_mean = np.concatenate([amplitude_mean, amplitude_mean_pred])
-    preds_std = np.concatenate([amplitude_std, amplitude_std_pred])
-    labels = np.array(["amplitude_target"] * N + ["amplitude_pred"] * N)
+        indices = torch.bucketize(wavenumbers_pred, bins)
+    radial_spectrum_profile = torch.zeros(len(bins))
+    radial_spectrum_profile_pred = torch.zeros(len(bins))
+    for i in range(1, len(bins)):
+        mask = indices == i
+        if torch.any(mask):
+            radial_spectrum_profile[i] = torch.sum(energy[mask])
+            radial_spectrum_profile_pred[i] = torch.sum(energy_pred[mask])
+    wavenumbers = np.concatenate([bins.numpy(), bins.numpy()])
+    spectrum = np.concatenate([radial_spectrum_profile.numpy(), radial_spectrum_profile_pred.numpy()])
+    labels = np.array(["energy_target"] * len(bins) + ["energy_pred"] * len(bins))
     data = pd.DataFrame({
         "wavenumber": wavenumbers,
-        "amplitude": preds_mean,
-        "pred_std": preds_std,
+        "spectrum energy": spectrum, 
         "label": labels 
+    })
+
+    return data
+
+
+def obtain_spectrum_sht(x, x_pred, idx):
+    nlat, nlon = x.shape[-2], x.shape[-1]
+    lmax = mmax = nlat
+    sht = RealSHT(nlat, nlon, lmax, mmax).to(x.device)
+    x_hat, x_pred_hat = sht(x), sht(x_pred)
+    amplitude, amplitude_pred = torch.abs(x_hat[idx,:,:]), torch.abs(x_pred_hat[idx,:,:]) 
+    energy, energy_pred = torch.sum(amplitude ** 2, dim=-1), torch.sum(amplitude_pred ** 2, dim=-1)
+    degrees = np.array([i for i in range(1, amplitude.shape[-1]+1)])
+    degrees_ = np.concatenate([degrees, degrees])
+    spectrum = np.concatenate([energy.numpy(), energy_pred.numpy()])
+    labels = np.array(["energy_target"] * len(degrees) + ["energy_pred"] * len(degrees))
+    data = pd.DataFrame({
+        "degree": degrees_,
+        "spectrum energy": spectrum,
+        "label": labels
     })
     return data
 
 
-"""
-input: dataframe obtained from the helper above 
-"""
-def obtain_freq_ranges(data):
-    N = len(data) // 2 
-    wavenumbers = data.wavenumber[:N]
-    upper, lower = data.amplitude + data.pred_std, data.amplitude - data.pred_std 
-    upper_target, lower_target = upper[:N], lower[:N]
-    upper_pred, lower_pred = upper[N:], lower[N:]
-    return wavenumbers, upper_target, lower_target, upper_pred, lower_pred
-
+def obtain_freq_vars(x, x_pred, idx, type="fft", n_bins=32):
+    x = torch.mean(x, dim=0).to("cpu")
+    x_pred = torch.mean(x_pred, dim=0).to("cpu")
+    if type == "fft":
+        return obtain_spectrum_fft(x, x_pred, idx, n_bins)
+    else:
+        return obtain_spectrum_sht(x, x_pred, idx)
 
 
 """
 a specific plotting function when there's only one feature
 """
-def one_step_plot_spectrum_single(pred_hat, pred_target, vars, model_name, predict_range=72, add_ribbon_bar=False, type="fft"):
+def one_step_plot_spectrum_single(x, x_pred, vars, model_name, predict_range=72, type="fft"):
     fig = plt.figure(figsize=(15,10))
-    data = obtain_freq_vars(pred_hat, pred_target, 0, type=type)
-    sns.lineplot(data=data, x="wavenumber", y="amplitude", hue="label")
-    plt.title(f"spectrum plot for channel {vars[0]}")
-    if add_ribbon_bar:
-        wavenumbers, lower_target_amplitude, upper_target_amplitude, lower_pred_amplitude, upper_pred_amplitude = obtain_freq_ranges(data)
-        plt.fill_between(wavenumbers, lower_pred_amplitude, upper_pred_amplitude)
-        plt.fill_between(wavenumbers, lower_target_amplitude, upper_target_amplitude)
-    plt.savefig(f"figs/spectrum/{model_name}/spectrum_plot_{model_name}_test_range={predict_range}_ribbon={add_ribbon_bar}_{type}.png")
+    data = obtain_freq_vars(x, x_pred, 0, type=type)
+    xlabel = "wavenumber" if type == "fft" else "degree"
+    sns.lineplot(data=data, x=xlabel, y="spectrum energy", hue="label")
+    plt.title(f"spectrum energy plot for channel {vars[0]}")
+    plt.savefig(f"figs/spectrum/{model_name}/spectrum_plot_{model_name}_test_range={predict_range}_{type}.png")
     plt.close()
 
 """
@@ -83,22 +101,11 @@ input: pred: Tensor (b, c, h, w)
        target: Tensor (b, c, h, w)
 """
 
-def one_step_plot_spectrum(pred, target, vars, model_name, batch_id = None, predict_range=72, add_ribbon_bar=False, type="fft"):
+def one_step_plot_spectrum(pred, target, vars, model_name, batch_id = None, predict_range=72, type="fft"):
     print(f"visualizing data of shape {target.shape}")
-    n_channels = pred.shape[1]
-    
-    if type == "fft":
-        pred_hat = fft.fftn(pred, dim=(-2,-1))
-        pred_target = fft.fftn(target, dim=(-2,-1))
-    else: # modified to be sht
-        nlat, nlon = target.shape[-2], target.shape[-1]
-        lmax = mmax = nlat
-        sht = RealSHT(nlat, nlon, lmax=lmax, mmax=mmax).to(pred.device)
-        pred_hat = sht(pred)  # (b,c,h,h)
-        pred_target = sht(target) # (b,c,h,h)
-        
+    n_channels = pred.shape[1]        
     if len(vars) == 1:
-        one_step_plot_spectrum_single(pred_hat, pred_target, vars, model_name, predict_range, add_ribbon_bar, type=type)
+        one_step_plot_spectrum_single(pred, target, vars, model_name, predict_range, type=type)
         return 
     n_rows = n_channels // 2
     fig, ax = plt.subplots(n_rows, 2)
@@ -107,27 +114,19 @@ def one_step_plot_spectrum(pred, target, vars, model_name, batch_id = None, pred
     for i in range(n_rows):
         idx1 = i * 2 # first plot index
         idx2 = i * 2 + 1  # second plot index, same row
-        data1 = obtain_freq_vars(pred_hat, pred_target, idx1, type=type)
-        ax1 = ax[i,0] if n_rows > 1 else ax[0]        
-        sns.lineplot(data=data1, ax=ax1, x="wavenumber", y="amplitude", hue="label")
+        data1 = obtain_freq_vars(pred, target, idx1, type=type)
+        ax1 = ax[i,0] if n_rows > 1 else ax[0]     
+        xlabel = "wavenumber" if type == "fft" else "degree"
+        sns.lineplot(data=data1, ax=ax1, x=xlabel, y="spectrum energy", hue="label")
         ax1.set_title(f"spectrum plot for channel {vars[idx1]}")
-        if add_ribbon_bar:
-            wavenumbers, lower_target_amplitude, upper_target_amplitude, lower_pred_amplitude, upper_pred_amplitude = obtain_freq_ranges(data1)
-            ax1.fill_between(wavenumbers, lower_pred_amplitude, upper_pred_amplitude)
-            ax1.fill_between(wavenumbers, lower_target_amplitude, upper_target_amplitude)
-
         # second plot, same row 
         if i * 2 + 1 >= n_channels: 
             break 
-        data2 = obtain_freq_vars(pred_hat, pred_target, idx2, type=type)
+        data2 = obtain_freq_vars(pred, target, idx2, type=type)
         ax2 = ax[i,1] if n_rows > 1 else ax[1]        
-        sns.lineplot(data=data2, ax=ax2, x="wavenumber", y="amplitude", hue="label")
+        sns.lineplot(data=data2, ax=ax2, x=xlabel, y="spectrum energy", hue="label")
         ax2.set_title(f"spectrum plot for channel {vars[idx2]}")
-        if add_ribbon_bar:
-            wavenumbers, lower_target_amplitude, upper_target_amplitude, lower_pred_amplitude, upper_pred_amplitude = obtain_freq_ranges(data2)
-            ax2.fill_between(wavenumbers, lower_pred_amplitude, upper_pred_amplitude)
-            ax2.fill_between(wavenumbers, lower_target_amplitude, upper_target_amplitude)
-    plt.savefig(f"figs/spectrum/{model_name}/spectrum_plot_{model_name}_{batch_id}_range={predict_range}_ribbon={add_ribbon_bar}_{type}.png")
+    plt.savefig(f"figs/spectrum/{model_name}/spectrum_plot_{model_name}_{batch_id}_range={predict_range}_{type}.png")
     plt.close()
 
 
@@ -145,12 +144,11 @@ TODO: add a power spectrum plot with bins
 """
 
 
-
-
 # tests 
 
 if __name__ == "__main__":
-    pred = torch.randn((32, 3, 64, 128))
-    target = torch.randn((32, 3, 64, 128))
-    vars = ["test_vars1", "test_vars2"]
-    one_step_plot_spectrum(pred, target, vars, "test", batch_id = 1, predict_range=72, add_ribbon_bar=False, type="sht")
+    pred = torch.randn((32, 1, 64, 128))
+    target = torch.randn((32, 1, 64, 128))
+    vars = ["test_vars1"]
+    # one_step_plot_spectrum(pred, target, vars, "test", batch_id = 1, predict_range=72, type="sht")
+    one_step_plot_spectrum(pred, target, vars, "test", batch_id = 1, predict_range=72, type="fft")
